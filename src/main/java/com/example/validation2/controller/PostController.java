7828e92425dc07695361fcbbae35d04d428f93bf
fullm <matoma1_@naver.com>
2024-04-20 01:30:39 +0900

    @Valid 어노테이션 추가

--- src/main/java/com/example/validation2/controller/PostController.java
+++ src/main/java/com/example/validation2/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.validation2.controller;
 
 import com.example.validation2.model.UserRegister;
 import com.example.validation2.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,14 +18,14 @@ public class PostController {
     private UserService userService;
 
     @PostMapping("/post1") //화면출력
-    public UserRegister saveUser(@RequestBody UserRegister userRegister){
+    public UserRegister saveUser(@Valid @RequestBody UserRegister userRegister){
         System.out.println(userRegister);
         userService.saveUser(userRegister);
         return userRegister;
     }
 
     @PostMapping("/post2")
-    public void saveUser2( @RequestBody UserRegister userRegister){
+    public void saveUser2(@Valid @RequestBody UserRegister userRegister){
         userService.saveUser(userRegister);
     }
 }
